a84f0a62c628041f093e77ea0e2bc416009a829f
Bingran You <bingran.you@berkeley.edu>
2025-02-17 14:48:07 -0800

    Create utils.py

---
+++ graphrag/utils.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GRAPHRAG_API_KEY = os.getenv("GRAPHRAG_API_KEY")
+GRAPHRAG_LLM_MODEL = os.getenv("GRAPHRAG_LLM_MODEL")
+GRAPHRAG_API_BASE = os.getenv("GRAPHRAG_API_BASE")
+GRAPHRAG_API_VERSION = os.getenv("GRAPHRAG_API_VERSION")
+
+def file_check_list(embedding_folder) -> list:
+    GraphRAG_embedding_folder = os.path.join(embedding_folder, "GraphRAG/")
+    create_final_community_reports_path = GraphRAG_embedding_folder + "output/create_final_community_reports.parquet"
+    create_final_covariates_path = GraphRAG_embedding_folder + "output/create_final_covariates.parquet"
+    create_final_document_path = GraphRAG_embedding_folder + "output/create_final_documents.parquet"
+    create_final_entities_path = GraphRAG_embedding_folder + "output/create_final_entities.parquet"
+    create_final_nodes_path = GraphRAG_embedding_folder + "output/create_final_nodes.parquet"
+    create_final_relationships_path = GraphRAG_embedding_folder + "output/create_final_relationships.parquet"
+    create_final_text_units_path = GraphRAG_embedding_folder + "output/create_final_text_units.parquet"
+    create_final_communities_path = GraphRAG_embedding_folder + "output/create_final_communities.parquet"
+    lancedb_path = GraphRAG_embedding_folder + "output/lancedb/"
+    path_list = [
+        create_final_community_reports_path,
+        create_final_covariates_path,
+        create_final_document_path,
+        create_final_entities_path,
+        create_final_nodes_path,
+        create_final_relationships_path,
+        create_final_text_units_path,
+        create_final_communities_path,
+        lancedb_path
+    ]
+    return path_list
